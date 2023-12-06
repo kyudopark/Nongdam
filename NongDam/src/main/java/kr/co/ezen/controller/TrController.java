acbf101718f04f0bad6c0417fad9ab55fa707381
Hye Kwan <cnlalddt@naver.com>
2023-12-06 18:03:41 +0900

    수정 글쓰기.

--- NongDam/src/main/java/kr/co/ezen/controller/TrController.java
+++ NongDam/src/main/java/kr/co/ezen/controller/TrController.java
@@ -1,6 +1,11 @@
 package kr.co.ezen.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -9,6 +14,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.oreilly.servlet.MultipartRequest;
+import com.oreilly.servlet.multipart.DefaultFileRenamePolicy;
 
 import kr.co.ezen.entity.Criteria;
 import kr.co.ezen.entity.PageCre;
@@ -59,9 +68,41 @@ public class TrController {
 	
 	@GetMapping("/write")
 	public String write() {
-		//게시글작성화면
 		
 		return "tr/write";
+	}
+	
+	@PostMapping("/write")
+	public String write(Tr vo,HttpServletRequest request, HttpSession session, RedirectAttributes rttr)
+			throws IOException{ 
+		MultipartRequest multi = null;
+		int fileSize = 40 * 1024 * 1024; // 10MB
+		String sPath = request.getRealPath("resources/image/tr");
+		
+		multi = new MultipartRequest(request, sPath, fileSize, "UTF-8", new DefaultFileRenamePolicy());
+		
+		String newPro="";
+		File file = multi.getFile("tr_imgpath");
+		if (file != null) {
+			String ext = file.getName().substring(file.getName().lastIndexOf(".") + 1);
+			ext = ext.toUpperCase();
+			if (ext.equals("PNG") || ext.equals("GIF") || ext.equals("JPG")) {
+				
+			
+			newPro=file.getName();
+			}else {
+				if(file.exists()) {
+					file.delete();
+				}
+				return "redirect:/tr/write";
+			}
+		}
+		vo.setTr_imgpath(newPro);
+		trService.insert(vo);
+
+		
+		return "redirect:/tr/main";
+				
 	}
 	
 	@PostMapping("/delete")
@@ -76,7 +117,22 @@ public class TrController {
 		return "redirect:/tr/main";
 	}
 	
+	@GetMapping("/modify")
+	public String modify(Model m,@RequestParam("tr_idx") int tr_idx) {
+		
+		
+		Tr vo=trService.findByIdx(tr_idx);
+		
+		m.addAttribute("vo",vo);
+		
+		return "/tr/modify";
+	}
 	
-	//--------------------------------------------------------
+	@PostMapping("/modify")
+	public String modify(Tr vo) {
+		
+		trService.updateByIdx(vo);
+		return "redirect:/tr/main";
+	}
 	
 }
