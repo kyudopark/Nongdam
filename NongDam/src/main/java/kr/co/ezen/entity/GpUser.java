db3931c7f7c4872d0abab442fe8ffa17cfb6645d
YJY1129 <wmekwm@gmail.com>
2023-12-08 16:07:58 +0900

    20231208 GP Commit

--- NongDam/src/main/java/kr/co/ezen/entity/GpUser.java
+++ NongDam/src/main/java/kr/co/ezen/entity/GpUser.java
@@ -1,5 +1,19 @@
 package kr.co.ezen.entity;
 
-public class GpUser {
+import java.util.Date;
 
+import lombok.Data;
+
+@Data
+public class GpUser {
+	
+	private int gp_idx;
+	private int user_idx;
+	
+	private String gp_addr;
+	private String gp_name;
+	private String gp_email;
+	private String gp_num;
+
+	
 }
